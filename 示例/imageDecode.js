a5f16d7d87124472c559ba4be19760861a75891f
xiaomianao <1596889407@qq.com>
2022-11-14 17:30:47 +0800

    Create imageDecode.js

---
+++ 示例/imageDecode.js
@@ -0,0 +1,14 @@
+d.push({
+	img: 'http://1.com/1.jpg@js=' + $.toString(()->{
+		let javaImport = new JavaImporter();
+		javaImport.importPackage(
+			Packages.com.example.hikerview.utils
+		);
+		with(javaImport) {
+			let bytes = FileUtil.toBytes(input);
+			//decryptData为解密方法
+			bytes = decryptData(bytes);
+			return FileUtil.toInputStream(bytes);
+		}
+	})
+})
